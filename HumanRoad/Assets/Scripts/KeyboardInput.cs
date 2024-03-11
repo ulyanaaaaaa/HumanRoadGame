c5bd9d2116638e444b2df71f004a65e2555a0e4d
Ulyana <ulianabober@gmail.com>
2024-03-11 22:31:49 +0300

    Add human

---
+++ HumanRoad/Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour
+{
+    public Action OnRightCliked;
+    public Action OnLeftCliked;
+    public Action OnBackCliKed;
+    public Action OnRunCliked;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+            OnRunCliked?.Invoke();
+        
+        if(Input.GetKeyDown(KeyCode.D))
+            OnRightCliked?.Invoke();
+        
+        if(Input.GetKeyDown(KeyCode.A))
+            OnLeftCliked?.Invoke();
+        
+        if(Input.GetKeyDown(KeyCode.S))
+            OnBackCliKed?.Invoke();
+    }
+}
